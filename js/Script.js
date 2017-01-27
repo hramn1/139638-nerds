04ae22683ab185d9b7dfd5825bd281f2a2ace9ca
hramn1 <stmihailov@yandex.ru>
2017-01-27 16:45:53 +0300

    JS

---
+++ js/Script.js
@@ -0,0 +1,18 @@
+function Slide1()
+{
+var S1 = document.getElementById("slide1").style.display="block";
+var S2 = document.getElementById("slide2").style.display="none";
+var S3 = document.getElementById("slide3").style.display="none";
+};
+function Slide2()
+{
+var S1 = document.getElementById("slide1").style.display="none";
+var S2 = document.getElementById("slide2").style.display="block";
+var S3 = document.getElementById("slide3").style.display="none";
+};
+function Slide3()
+{
+var S1 = document.getElementById("slide1").style.display="none";
+var S2 = document.getElementById("slide2").style.display="none";
+var S3 = document.getElementById("slide3").style.display="block";
+};
